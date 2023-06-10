9575293a95583171f2ba669127367119f7a7e367
Eduardo Castro <tec.eduardocastro@gmail.com>
2023-06-09 23:39:12 -0300

    feat: populate db

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,12 +1,7 @@
-import express from 'express'
-
 import { Router, Request, Response } from 'express';
-
-const app = express();
+import app from './app';
 
 const route = Router()
-
-app.use(express.json())
 
 route.get('/', (req: Request, res: Response) => {
   res.json({ message: 'hello world with Typescript' })
